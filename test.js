d34a1b8a0d9c7b2d10ba78b2ad5bd67893046b35
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-04-26 15:21:15 +0900

    add more tests to cover various cases

--- test.js
+++ test.js
@@ -1,23 +1,57 @@
 'use strict';
-var fs = require('fs');
 var assert = require('assert');
+var path = require('path');
+var File = require('vinyl');
 var gutil = require('gulp-util');
+var read = require('vinyl-file').read;
 var webp = require('./');
 
 it('should convert images to WebP', function (cb) {
-	var LOSSY_SIZE = 20000;
-	var stream = webp({lossless: true});
+	read(path.join(__dirname, 'fixture.jpg'), function(err, file) {
+		assert.strictEqual(err, null);
 
-	stream.once('data', function (file) {
-		assert(file.contents.length > LOSSY_SIZE);
+		var stream = webp();
+		var size = file.contents.length;
+
+		stream.on('data', function (data) {
+			assert(data.contents.length < size);
+			assert.equal(data.path, path.join(__dirname, 'fixture.webp'));
+		});
+
+		stream.on('end', cb);
+
+		stream.end(file);
+	});
+});
+
+it('should not convert unsupported files', function (cb) {
+	var stream = webp();
+
+	stream.on('data', function (data) {
+		assert.equal(String(data.contents), 'contents');
+		cb();
 	});
 
-	stream.on('end', cb);
-
-	stream.write(new gutil.File({
-		path: __dirname + '/fixture.png',
-		contents: fs.readFileSync('fixture.jpg')
+	stream.end(new File({
+		contents: new Buffer('contents')
 	}));
+});
 
-	stream.end();
+it('should emit a plugin error when the image is corrupt', function (cb) {
+	var fileName = path.join(__dirname, 'fixture-corrupt.webp');
+
+	read(fileName, function(err, file) {
+		assert.strictEqual(err, null);
+
+		var stream = webp();
+
+		stream.on('error', function (err) {
+			assert(err instanceof gutil.PluginError);
+			assert(err.plugin, 'gulp-webp');
+			assert.equal(err.fileName, fileName);
+			cb();
+		});
+
+		stream.end(file);
+	});
 });
